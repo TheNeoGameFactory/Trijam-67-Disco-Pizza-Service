c01d5ecb14a3eb0b29946d69229f099f7cf91837
Christoph Winkler <support@neo-gamefactory.de>
2020-04-26 22:18:49 +0200

    Pizza geschos + Game Start

--- Disco Pizza Service/scenes/main_level/main_level.gd
+++ Disco Pizza Service/scenes/main_level/main_level.gd
@@ -4,11 +4,16 @@ extends Node2D
 signal music_stoped
 signal music_started
 
+onready var pizzaGeschoss =preload("res://scenes/Geschoss/PizzaGeschoss.tscn")
+
 
 
 
 func _on_start_music_button_down():
 	emit_signal("music_started")
+	$PizzaGeschoss.queue_free()
+	$"Demo Level".show()
+	$start_music.hide()
 
 
 
@@ -22,7 +27,7 @@ func _on_sound_detectorplayer_barrikaden_spawn_now():
 	pass
 
 func _on_sound_detectorplayer_farbaenderung_now():
-	#$AnimationPlayer.play("blink_colorRect")
+	$AnimationPlayer.play("blink_colorRect")
 	pass
 
 func _on_sound_detectorplayer_gegner_spawn_now():
@@ -31,5 +36,9 @@ func _on_sound_detectorplayer_gegner_spawn_now():
 
 
 func _on_sound_detectorplayer_schiesen_now():
-	$AnimationPlayer.play("blink_colorRect")
+	#$AnimationPlayer.play("blink_colorRect")
+	var pizza = pizzaGeschoss.instance()
+	var origin = $"Demo Level/Car".position
+	add_child(pizza)
+	pizza.init(origin,get_local_mouse_position())
 	pass
