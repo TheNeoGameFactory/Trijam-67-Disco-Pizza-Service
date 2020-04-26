3000669c15def9e5541c95129f8b7e4580c55b01
unknown <alich0107@gmail.com>
2020-04-26 13:30:09 +0200

    Fahrphysik fertig

--- Disco Pizza Service/Scripts/Car/car.gd
+++ Disco Pizza Service/Scripts/Car/car.gd
@@ -4,20 +4,19 @@ var MAX_SPEED = 500
 var ACCELERATION = 1000
 var motion = Vector2.ZERO
 
-func _physics_process(delta):
-	var axis = get_input_axis()
-	if axis == Vector2.ZERO:
-		apply_friction(ACCELERATION * delta)
-	else:
-		apply_movement(axis * ACCELERATION  * delta)
-	if (motion.x < 1 && motion.x > -1):
-		motion.y = 0
-	motion = move_and_slide(motion)
+func _ready():
+	set_process(true)
+#	set_process_input(true)
+
+#func _input_event(viewport, event, shape_idx):
+#	if event is InputEventMouseMotion:
+#		var mouse_position = get_global_mouse_position()
+#		print(mouse_position)
 
 func get_input_axis():
 	var axis = Vector2.ZERO
 	axis.x = int(Input.is_action_pressed("car_throttle")) - int(Input.is_action_pressed("car_brake"))
-	axis.y = int(Input.is_action_pressed("car_right")) - int(Input.is_action_pressed("car_left")) * 0.05
+	axis.y = int(Input.is_action_pressed("car_right")) - int(Input.is_action_pressed("car_left"))
 	return axis.normalized()
 
 func apply_friction(amount):
@@ -30,3 +29,13 @@ func apply_movement(acceleration):
 	motion += acceleration
 	motion.y *= 0.9
 	motion = motion.clamped(MAX_SPEED)
+
+func _physics_process(delta):
+	var axis = get_input_axis()
+	if axis == Vector2.ZERO:
+		apply_friction(ACCELERATION * delta)
+	else:
+		apply_movement(axis * ACCELERATION  * delta)
+	if (motion.x < 1 && motion.x > -1):
+		motion.y = motion.x
+	motion = move_and_slide(motion)
