95cf5a1d2af5f09a0b17e9030ba2feae45d01122
Christoph Winkler <support@neo-gamefactory.de>
2020-04-26 22:52:05 +0200

    Barrikade, Mauscursor, kollision Barrikade + bewegung

---
+++ Disco Pizza Service/Scenes/Barrikade/Barrikade.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(Vector2(-8,0))
+	pass
+
+
+func _on_Barrikade_body_entered(body):
+	if body.get_name() == "Car":
+		queue_free()
+	pass # Replace with function body.
